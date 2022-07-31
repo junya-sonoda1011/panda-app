ff34a93620fe4206cd4fb342ea84aae427e77c8a
jpnm-panda <jpnm0415shkm@gmail.com>
2022-07-31 20:27:10 +0900

    クラスバリデーションを追加

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,11 @@
 // import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
